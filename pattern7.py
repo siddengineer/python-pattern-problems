7b95287b32bcefe1a28e136163b6dc1f7ac925af
siddengineer <siddharthpatil7381@gmail.com>
2025-03-19 09:41:57 +0530

    Add files via upload
    * * * * * 
     * * * * 
      * * * 
       * *

---
+++ pattern7.py
@@ -0,0 +1,13 @@
+# p_height = 5
+# for i in range (p_height, 0,-1):
+#     for k in range(p_height - 1,i - 1, -1):
+#         print(" ",end="")
+#     for j in range(i,0,-1):
+#         print("* ",end="")
+#     print()        
+
+# * * * * * 
+#  * * * * 
+#   * * * 
+#    * * 
+#     * 
